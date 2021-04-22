cf4632270b7329085f31e980d19325c0585d25f7
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-23 02:53:14 +0530

    Added to check Attendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,1 +1,13 @@
-# welcome to employee wage computation
+#!/bin/bash -x
+
+echo "Welcome to Employee Wage Computation Program!"
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [[ $isPresent -eq $randomCheck ]]
+then
+        echo "Employee is Present";
+else
+        echo "Employee is not Present"
+fi
